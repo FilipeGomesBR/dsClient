3ba9a52238a35aff7a72598ce4d9967830ccd18e
Filipe Gomes <filipe.gomes@atos.net>
2022-01-25 22:49:58 -0300

    Desafio dsclient completo

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,24 @@
+
+
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Carlos Souza Filho','823.589.654-05' ,1290.0, TIMESTAMP WITH TIME ZONE '1987-01-23T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Paulo Farias Valetin','142.589.400-11' ,4590.0, TIMESTAMP WITH TIME ZONE '1984-05-13T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Bruno Almeida campos','563.589.654-22' ,9590.0, TIMESTAMP WITH TIME ZONE '1988-07-23T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Rogerio Martins Perreira','825.259.654-12' ,1490.0, TIMESTAMP WITH TIME ZONE '1987-07-03T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Filipe Paulo Soare','535.589.654-30' ,1290.0, TIMESTAMP WITH TIME ZONE '1988-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Rebeca Maria Lins','222.589.654-22' ,3260.0, TIMESTAMP WITH TIME ZONE '1989-08-11T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Valerio Soares Dutra','232.589.654-56' ,1220.0, TIMESTAMP WITH TIME ZONE '2000-03-13T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Gustavo lima mendonça','287.589.654-52' ,1420.0, TIMESTAMP WITH TIME ZONE '1999-12-13T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Caio pereira neto','363.582.654-87' ,4590.0, TIMESTAMP WITH TIME ZONE '1980-05-13T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Taciano Jose de souza','656.589.654-48' ,1290.0, TIMESTAMP WITH TIME ZONE '1981-09-22T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Ferreira Neto Martins','545.589.654-42' ,1590.0, TIMESTAMP WITH TIME ZONE '1987-01-12T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Carla Jessica souza','564.589.654-38' ,3290.0, TIMESTAMP WITH TIME ZONE '1982-02-13T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Joana Lira lima','823.435.654-47' ,3210.0, TIMESTAMP WITH TIME ZONE '1999-04-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Carlos filho neto','823.545.654-25' ,4990.0, TIMESTAMP WITH TIME ZONE '1980-01-23T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Carlos Gomes da silva','454.589.654-35' ,2090.0, TIMESTAMP WITH TIME ZONE '1987-01-25T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Filipe Souza Neto','823.111.654-34' ,1290.0, TIMESTAMP WITH TIME ZONE '1987-01-01T20:50:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Nelio Alves Filho','823.145.654-42' ,1190.0, TIMESTAMP WITH TIME ZONE '1997-07-01T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Maria Dolores Ribeiro','259.589.654-02' ,1290.0, TIMESTAMP WITH TIME ZONE '1997-10-13T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Andrea Cristina de Souza','445.589.654-58' ,4490.0, TIMESTAMP WITH TIME ZONE '1999-11-15T20:50:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Josilda Maria da Conceição','450.589.654-14' ,3990.0, TIMESTAMP WITH TIME ZONE '2000-12-13T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Celia Soares Dutra','258.333.654-11' ,1390.0, TIMESTAMP WITH TIME ZONE '1996-05-11T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date,children) VALUES ('Paulo Amorin Dutra','453.452.543-10' ,3890.0, TIMESTAMP WITH TIME ZONE '1998-02-15T20:50:07.12345Z', 1);
